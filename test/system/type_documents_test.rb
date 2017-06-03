6b5246d8e43e1af2e591cdf55ce56e004b16cbdf
Paola Cuadros <paola.cuadroso@gmail.com>
2017-06-03 20:42:10 +0000

    Agregar imagen de la aplicación y eliminación de archivos

---
+++ test/system/type_documents_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class TypeDocumentsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit type_documents_url
+  #
+  #   assert_selector "h1", text: "TypeDocument"
+  # end
+end
